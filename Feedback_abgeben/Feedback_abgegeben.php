40decc336ee2b395bc48d96bd7c6d7a3d3ea101a
Holdo89 <64363703+Holdo89@users.noreply.github.com>
2021-08-30 23:40:08 +0200

    feedback_abgegeben style

--- Feedback_abgeben/Feedback_abgegeben.php
+++ Feedback_abgeben/Feedback_abgegeben.php
@@ -1,5 +1,6 @@
 <?php
 // Start the session
+require_once "../config.php";
 $cookie_name = "feedback_abgegeben";
 $cookie_value = 1;
 setcookie($cookie_name, $cookie_value, time() + (86400), "/"); // 86400 = 1 day
@@ -7,7 +8,8 @@ session_start();
 $_SESSION["Fragenindex"] = -1;
 $_SESSION["Helpindex"] = 1;
 $_SESSION["Fragen"] = array();
-require_once "../config.php";
+$dir = '../assets/'.$subdomain.'/logo/';
+$file = scandir($dir);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,37 +18,33 @@ require_once "../config.php";
 ?>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Feedback abgeben</title>
 </head>
 <link href = "css/fancy.css" type = "text/css" rel = "stylesheet" /> 
-<?php
-$dir = "../assets/".$subdomain."/bg_givefb/";
-$dir_logo = "../assets/".$subdomain."/logo/";
-$file = scandir($dir);
-$file_logo = scandir($dir_logo);
-echo"
-<body style=\"background-image:url('../assets/".$subdomain."/bg_givefb/".$file[2]."')\";>
-<div style='width:40vw; min-width:900px;'>
-    <img class= 'center' src='../assets/".$subdomain."/logo/".$file_logo[2]."' alt='' width='400' height='140'>
-    <label style='font-size: 16pt; color:white; padding:10vw; text-align:center;'> 
-";
-if($_SESSION["Sprache"]=="Deutsch")
-{
-	$sql = "SELECT Text_nach_Abgabe FROM system";
-	$query = mysqli_query($link, $sql);
-	$result = mysqli_fetch_array($query);
-	echo $result["Text_nach_Abgabe"]."</label>
-    ";
-}
-else if($_SESSION["Sprache"]=="Englisch")
-{
-	$sql = "SELECT Text_after_Feedback FROM system";
-	$query = mysqli_query($link, $sql);
-	$result = mysqli_fetch_array($query);
-	echo $result["Text_after_Feedback"]."</label>
-    ";
-}
-?>
+<body id ='body' style= "background-image: url('../assets/<?php echo $subdomain ?>/bg_givefb/<?php $dir = "../assets/".$subdomain."/bg_givefb/";$file_background = scandir($dir);echo $file_background[2];?>');">
+<form id='Frage_form' style='margin:30px;min-height:600px;'>
+</form>
 </div>
 </body>
+<script>
+document.getElementById("Frage_form").innerHTML=<?php echo"\"<img class='center' src='../assets/".$subdomain."/logo/".$file[2]."' alt='' width='150' height='70'> <p style='text-align:center'>"; 
+        if($_SESSION["Sprache"]=="Deutsch")
+        {
+            $sql = "SELECT Text_nach_Abgabe FROM system";
+            $query = mysqli_query($link, $sql);
+            $result = mysqli_fetch_array($query);
+            echo $result["Text_nach_Abgabe"]."</p>";
+        }
+        else if($_SESSION["Sprache"]=="Englisch")
+        {
+            $sql = "SELECT Text_after_Feedback FROM system";
+            $query = mysqli_query($link, $sql);
+            $result = mysqli_fetch_array($query);
+            echo $result["Text_after_Feedback"]."</p>";
+        }
+        echo "<img class='center' src='undraw_going_up.svg' alt='' width='300' style='margin-top:10vh;'> <p style='text-align:center; font-size:13px;'>Powered by <a href='https://feedcube.net'>Feedcube.net</a></p> \" ";
+        ?>;
+</script>
 </html>
+
