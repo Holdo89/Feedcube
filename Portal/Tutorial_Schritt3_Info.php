1a493f49e85310472fb5fe20a9a0518e21df824c
Holdo89 <d.holzweber@hotmail.com>
2022-03-17 15:29:37 +0100

    Tutorial Umfrage

--- Portal/Tutorial_Schritt3_Info.php
+++ Portal/Tutorial_Schritt3_Info.php
@@ -5,7 +5,7 @@
 		<div>		
 		<h4 style="font-size:30px; margin-bottom:10px;"><img src="../assets/brand/questionmark.png" width="60" style="margin-top:-10px;">Fragen </h4>
 		<p style="text-align:left">
-		    Hier werden bestehende Fragen bearbeitet und neue Fragen hinzugefügt, die bei Kundenbefragungen beantwortet werden. Dabei kann zwischen 4 verschiedenen Fragentypen unterschieden werden.   
+		    Hier werden bestehende Fragen bearbeitet und neue Fragen hinzugefügt, die bei Kundenbefragungen beantwortet werden. Dabei wird zwischen 4 verschiedenen Fragentypen unterschieden.   
         </p>
         <p style="text-align:left">
         <b>Bewertung</b> - Fragen bei denen genau eine Antwortmöglichkeit gewählt wird. <a onclick="showExample('radio')">Beispiel anzeigen</a><br><br></p>
@@ -37,9 +37,6 @@
 			</label>
 		</div>
 		<p style="text-align:left">
-		<i style="font-size:20px; color:blue" class="fas fa-info"></i> Für die Antwortmöglichkeiten bei Bewertungen und Multiplechoice	Fragen können vordefinierte Optionen gewählt werden, oder fragenspezifische Antwortmöglichkeiten erstellt werden, die nicht bei anderen Fragen wiederverwendet werden.  
-		</p>
-		<p style="text-align:left">
         <b>Schieberegler</b> - Durch Verschieben eines Reglers wird ein Zahlenwert ermittelt und als Bewertung abgegeben. Um das Maximum und Minimum des Reglers zu bearbeiten klicke <i class="fa fa-pencil" aria-hidden="true"></i>. <a onclick="showExample('range')">Beispiel anzeigen</a><br><br></p>
 		<div id="range" class="radio">
 		<div style='text-align:center'><p style='margin-bottom:20px;'>Bewertung: <span id='output' style='font-size:15px;'>50</span></p><input type='range' style='width:70%; margin:auto;' min='0' max='100' value='50' name='element_1_1' id='element_1_1' ontouchend='input_update(1)' oninput='input_update(1), color(1)'></div>
@@ -47,6 +44,7 @@
 		</div>
 		<p style="text-align:left">
 		<b>Text</b> - Fragen werden durch ein Kommentar in Textform beantwortet.<br><br>
+		<i style="font-size:20px; color:blue" class="fas fa-info"></i> Für die Antwortmöglichkeiten bei Bewertungen und Multiplechoice Fragen können vordefinierte Optionen gewählt, oder fragenspezifische Antworten erstellt werden.  
 		Um eine bestehende Frage zu bearbeiten, klicke <i class="fa fa-pencil" aria-hidden="true"></i>.
 		</p>
 		    <button id="element" style="font-size:13px; margin-top:20px; width:170px; padding:10px;"><i class="fa fa-check" aria-hidden="true" style="font-size:13px"></i> Alles klar</button>
--- Portal/Tutorial_Schritt7_Info.php
+++ Portal/Tutorial_Schritt7_Info.php
@@ -4,21 +4,16 @@
 	    <div></div>
 		<span class="close" onclick="hideinformationWithoutremembering()" style="text-align:right">&times;</span>
 		<div>		
-		<h4 style="font-size:30px; margin-bottom:10px;"><img src="../assets/brand/group.png" width="60"> Benutzer </h4>
+		<h4 style="font-size:30px; margin-bottom:10px;"><img src="../assets/brand/promotion.png" width="60"> Umfragen </h4>
 		<p style="text-align:left">
-		    Hier werden neue Benutzer hinzugefügt und verwaltet. Zu jedem Benutzer stehen weitere Optionen zur Verfügung. <br>
+		    Hier werden Umfragen hinzugefügt und verwaltet. Sowohl interne Mitarbeiter, als auch externe Kunden können in bestimmten zeitlichen Abständen an Umfragen teilnehmen. Per Email werden gespeicherte Benutzer benachrichtigt, die an der Umfrage teilnehmen. Bei Umfragen werden jeweils eigene Fragen hinzugefügt. Zudem stehen folgende Optionen zur Verfügung:<br>
 		</p>
 			<p style="text-align:left; line-height:30px;">
-				<i class='fa fa-lock'></i> neues Passwort vergeben.<br>	
-				<i class='fa fa-trash'></i> ausgewählten Benutzer löschen. <br>
-				<i class='fa fa-link'></i> Feedbacklink für ausgewählten Benutzer erstellen.<br>	   
+				<i class='fa fa-link'></i> Link zur ausgewählten Umfrage erstellen.<br>	 
+				<i class='fa fa-pencil'></i> Umfrage bearbeiten.<br>	     
+				<i class='fa fa-trash'></i> Umfrage löschen. <br>
 			</p>	
-		<p style="text-align:left">
-		Des weiteren werden Benutzer in zwei Gruppen unterschieden.<br><br>
-		<i class="fas fa-user-cog"></i> Administratoren konfigurieren das System und besitzen die Berechtigung neue Fragen, Antworten, Leistungen, Benutzer und Umfragen hinzuzufügen. Zudem besitzen Administratoren zusätzliche Möglichkeiten um abgegebenes Feedback auszuwerten bzw. zu löschen.  
-		<br><br><i class="fas fa-chalkboard-teacher"></i> Trainer sind Benutzer die an Umfragen teilnehmen und zu denen Feedback abgegeben werden kann. Trainer besitzen eingeschränkte Möglichkeiten bei der Auswertung von Feedback. So ist es für diese Benutzergruppe nicht möglich bei der Auswertung des Feedbacks nach anderen Trainern zu filtern oder abgegebenes Feedback zu löschen.	
-	</p>
-		    <button id="element" style="font-size:13px; margin-top:20px; width:170px; padding:10px;"><i class="fa fa-check" aria-hidden="true" style="font-size:13px"></i> Alles klar</button>
+	   <button id="element" style="font-size:13px; margin-top:20px; width:170px; padding:10px;"><i class="fa fa-check" aria-hidden="true" style="font-size:13px"></i> Alles klar</button>
 		</div>
 
 	</form>
