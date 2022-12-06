b420f19904586b197f9cf68d1983648df7bfc188
Holdo89 <d.holzweber@hotmail.com>
2022-12-06 21:34:02 +0100

    Leistungen Tutorial

--- Portal/Tutorial_Schritt5_Info.php
+++ Portal/Tutorial_Schritt5_Info.php
@@ -4,10 +4,10 @@
 	    <div></div>
 		<span class="close" onclick="hideinformationWithoutremembering()" style="text-align:right">&times;</span>
 		<div style="text-align:center">		
-		<h4 style="font-size:30px; margin-bottom:10px; text-align:center"><img src="../assets/brand/graduation-hat.png" width="60"> Leistungen </h4>
+		<h4 style="font-size:30px; margin-bottom:10px; text-align:center"><img src="../assets/brand/graduation-hat.png" width="60"> Kursliste</h4>
 		<p style="text-align:left">
-		    Leistungen repräsentieren Dienstleistungen zu denen Feedback von Teilnehmern abgegeben werden. Beispiele dafür sind Schulungen, Seminare, oder auch Beratungen. 
-		    Um Fragen zu wählen die bei einer Leistung abgefragt werden klicken Sie <i class="fa fa-pencil" aria-hidden="true"></i>. Fragen können einzeln oder durch
+			Füge Kurse, Schulungen, Seminare, oder Workshops der Kursliste hinzu, damit Kursteilnehmer Feedback dazu abgeben können. 
+		    Um Fragen zu wählen die bei einem Kurs abgefragt werden klicken Sie <i class="fa fa-pencil" aria-hidden="true"></i>. Fragen können einzeln oder durch
 		    ein zuvor definiertes Fragenset gewählt werden.  
 		</p>
 		    <button id="element" style="font-size:13px; margin-top:20px; width:170px; padding:10px;"><i class="fa fa-check" aria-hidden="true" style="font-size:13px"></i> Alles klar</button>
