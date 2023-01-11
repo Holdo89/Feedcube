ac4c503efd8ee5c2a49f46fa255d28c77967649f
Holdo89 <d.holzweber@hotmail.com>
2023-01-12 00:15:50 +0100

    small change Alle Trainer

--- Portal/Auswahlmöglichkeiten_Trainer.php
+++ Portal/Auswahlmöglichkeiten_Trainer.php
@@ -25,7 +25,7 @@ if ($i<1)
 else
 {
     echo' <select class="Auswahl_Select" id="Auswahl_Trainer" name="Auswahl_Trainer" oninput="update();">';
-    echo '<option value="%25">Alle Bewertungen</option><option value="">Keine Angabe</option>';
+    echo '<option value="%25">Alle Trainer</option><option value="">Keine Angabe</option>';
 
     $sql = "SELECT name, username, Is_Trainer FROM users WHERE Is_Trainer = 1 ORDER BY name ASC" ;
 
